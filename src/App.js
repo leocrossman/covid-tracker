d986ce893761c24b9cda07504d5070cc0cdf04cb
Leo Crossman <lkcrossman@gmail.com>
2020-05-28 16:22:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends React.Component {
+	async componentDidMount() {
+		const data = await fetchData();
+
+		console.log(data);
+	}
+
 	render() {
 		return (
 			<div className={styles.container}>
