3fc2eb9aa9e7bcdb7f9ecf61e7b8a5d0aabac13d
Leo Crossman <lkcrossman@gmail.com>
2020-05-28 16:41:30 -0400

    added material ui and card typography

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import {
+	Card,
+	CardContent,
+	Typography,
+	Grid,
+	StylesProvider,
+} from '@material-ui/core';
 
-const Cards = () => {
-	return <h1>Cards</h1>;
+import styles from './Cards.module.css';
+
+const Cards = props => {
+	console.log(props);
+	return (
+		<div className={styles.container}>
+			<Grid container spacing={3} justify='center'>
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color='textSecondary' gutterBottom>
+							Infected
+						</Typography>
+						<Typography variant='h5'> REAL DATA</Typography>
+						<Typography color='textSecondary'>REAL DATE</Typography>
+						<Typography variant='body2'>
+							Number of active cases of COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default Cards;
